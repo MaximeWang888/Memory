a5a575b500cf231b7978c2cdef8f80d48f04abdd
BladeBuru <72576887+BladeBuru@users.noreply.github.com>
2021-04-30 14:46:50 +0200

    Début Projet

---
+++ Projet-memory/Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ButtonJouer = New System.Windows.Forms.Button()
+        Me.ButtonOptions = New System.Windows.Forms.Button()
+        Me.ButtonScores = New System.Windows.Forms.Button()
+        Me.ButtonQuitter = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(104, 138)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'ButtonJouer
+        '
+        Me.ButtonJouer.Location = New System.Drawing.Point(214, 200)
+        Me.ButtonJouer.Name = "ButtonJouer"
+        Me.ButtonJouer.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonJouer.TabIndex = 1
+        Me.ButtonJouer.Text = "Jouer"
+        Me.ButtonJouer.UseVisualStyleBackColor = True
+        '
+        'ButtonOptions
+        '
+        Me.ButtonOptions.Location = New System.Drawing.Point(214, 243)
+        Me.ButtonOptions.Name = "ButtonOptions"
+        Me.ButtonOptions.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonOptions.TabIndex = 2
+        Me.ButtonOptions.Text = "Options"
+        Me.ButtonOptions.UseVisualStyleBackColor = True
+        '
+        'ButtonScores
+        '
+        Me.ButtonScores.Location = New System.Drawing.Point(214, 291)
+        Me.ButtonScores.Name = "ButtonScores"
+        Me.ButtonScores.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonScores.TabIndex = 3
+        Me.ButtonScores.Text = "Scores"
+        Me.ButtonScores.UseVisualStyleBackColor = True
+        '
+        'ButtonQuitter
+        '
+        Me.ButtonQuitter.Location = New System.Drawing.Point(214, 336)
+        Me.ButtonQuitter.Name = "ButtonQuitter"
+        Me.ButtonQuitter.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonQuitter.TabIndex = 4
+        Me.ButtonQuitter.Text = "Quitter"
+        Me.ButtonQuitter.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(189, 135)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(233, 20)
+        Me.TextBox1.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Bauhaus 93", 40.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(42, 34)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(272, 62)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "4 Memory"
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Bauhaus 93", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(211, 96)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(149, 16)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "a concentration game"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(546, 413)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ButtonQuitter)
+        Me.Controls.Add(Me.ButtonScores)
+        Me.Controls.Add(Me.ButtonOptions)
+        Me.Controls.Add(Me.ButtonJouer)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ButtonJouer As Button
+    Friend WithEvents ButtonOptions As Button
+    Friend WithEvents ButtonScores As Button
+    Friend WithEvents ButtonQuitter As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Projet-memory/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button_Quitter_Click(sender As Object, e As EventArgs) Handles ButtonQuitter.Click
+        Dim res As Integer = MsgBox("Voulez vous quittez", vbQuestion + vbYesNo + vbDefaultButton2, "Quitter")
+
+        If res = 6 Then
+            'Me.Close()
+            End
+        ElseIf res = 7 Then
+
+        End If
+    End Sub
+End Class
