cfaa73bbbe33c7b0832802477c826572b8260bfb
BladeBuru <72576887+BladeBuru@users.noreply.github.com>
2021-05-11 12:35:03 +0200

    Revert "Add files via upload"
    This reverts commit f3b13d69911e51125969baea72d0a492589ba12e.

--- Projet-memory/Score.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Score
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        DataGridView1.Rows(0).Cells("Column1").Value = "Miriam"
-        DataGridView1.Rows.Add()
-    End Sub
-End Class
